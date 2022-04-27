59aa9c564cd4a9adb6422d070d983613a9255cfb
DoTanTu <tudo7868@gmail.com>
2022-04-27 16:25:06 +0700

    lan 1

---
+++ src/javaapplication25/JavaApplication25.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication25;
+
+/**
+ *
+ * @author ACER
+ */
+public class JavaApplication25 {
+    private String name;
+    private int tuoi;
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
